5e4ce05686182f445d452ba6c800778f26c8f1da
Mohammed Abdul Rahman <96978499+AbdulRahman788@users.noreply.github.com>
2023-08-10 01:35:38 +0530

    celestial-conquest

---
+++ README.md
@@ -0,0 +1,26 @@
+# Celestial Conquest
+
+Welcome to Celestial Conquest, a classic arcade-style space invader game built with HTML, CSS, and JavaScript.
+
+## Gameplay
+
+Control your shooter using the arrow keys and shoot down the alien invaders before they reach the bottom. Be careful not to let them get too close!
+
+## Features
+
+- Classic space invader gameplay
+- Shooter gun icon
+- Sound effects for shooting and explosions
+- Dynamic movement of invaders
+- Victory and defeat conditions
+
+## Getting Started
+
+1. Clone the repository: `git clone https://github.com/AbdulRahman788/celestial-conquest.git`
+2. Open `index.html` in your web browser to start playing!
+
+## Copyright
+
+Copyright(c) 2023 Ash
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ app.js
@@ -0,0 +1,163 @@
+const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+let aliensRemoved = []
+let results = 0
+
+for (let i = 0; i < 225; i++) {
+  const square = document.createElement('div')
+  grid.appendChild(square)
+}
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+const alienInvaders = [
+  // First row
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+
+  // Second row
+  15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+
+  // Third row
+  30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+
+  // Fourth row
+  45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+
+  // Fifth row
+  60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+
+  // Sixth row
+  75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+
+  // // Seventh row
+  // 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+
+  // // Eighth row
+  // 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+
+  // // Ninth row
+  // 120, 121, 122, 123, 124, 125, 126, 127, 128, 129
+];
+
+
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    if (!aliensRemoved.includes(i)) {
+      squares[alienInvaders[i]].classList.add('invader')
+    }
+  }
+}
+
+draw()
+
+function remove() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.remove('invader')
+  }
+}
+
+squares[currentShooterIndex].classList.add('shooter')
+
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove('shooter')
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
+      break
+    case 'ArrowRight':
+      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1
+      break
+  }
+  squares[currentShooterIndex].classList.add('shooter');
+  // squares[currentShooterIndex].style.transform = `translateX(${currentShooterIndex % width * 20}px)`;
+}
+document.addEventListener('keydown', moveShooter)
+
+function moveInvaders() {
+  const leftEdge = alienInvaders[0] % width === 0
+  const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1
+  remove()
+
+  if (rightEdge && goingRight) {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += width + 1
+      direction = -1
+      goingRight = false
+    }
+  }
+
+  if (leftEdge && !goingRight) {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      alienInvaders[i] += width - 1
+      direction = 1
+      goingRight = true
+    }
+  }
+
+  for (let i = 0; i < alienInvaders.length; i++) {
+    alienInvaders[i] += direction
+  }
+
+  draw()
+
+  if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+    resultsDisplay.innerHTML = 'GAME OVER'
+    clearInterval(invadersId)
+  }
+
+  for (let i = 0; i < alienInvaders.length; i++) {
+    if (alienInvaders[i] > (squares.length)) {
+      resultsDisplay.innerHTML = 'GAME OVER'
+      clearInterval(invadersId)
+    }
+  }
+  if (aliensRemoved.length === alienInvaders.length) {
+    resultsDisplay.innerHTML = 'YOU WIN'
+    clearInterval(invadersId)
+  }
+}
+invadersId = setInterval(moveInvaders, 600)
+
+function shoot(e) {
+  let laserId
+  let currentLaserIndex = currentShooterIndex
+
+  document.getElementById("laserSound").play();
+
+  function moveLaser() {
+    squares[currentLaserIndex].classList.remove('laser')
+    currentLaserIndex -= width
+    squares[currentLaserIndex].classList.add('laser')
+
+    if (squares[currentLaserIndex].classList.contains('invader')) {
+      squares[currentLaserIndex].classList.remove('laser')
+      squares[currentLaserIndex].classList.remove('invader')
+      squares[currentLaserIndex].classList.add('boom')
+
+      setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 300)
+      clearInterval(laserId)
+
+      document.getElementById("explosionSound").play();
+
+      const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+      aliensRemoved.push(alienRemoved)
+      results++
+      resultsDisplay.innerHTML = results
+      console.log(aliensRemoved)
+
+    }
+
+  }
+  switch (e.key) {
+    case 'ArrowUp':
+      laserId = setInterval(moveLaser, 100)
+  }
+}
+
+document.addEventListener('keydown', shoot)
